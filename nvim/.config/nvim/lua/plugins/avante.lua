86abcf6728bf0abe8c4aeb69f297c6f044e7beea
limymy <921366814@qq.com>
2025-05-26 19:25:26 +0800

    nvim：使用avante替换copilot-chat插件

---
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,74 @@
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    opts = {
+      provider = "copilot",
+      auto_suggestions_provider = "copilot",
+      copilot = {
+        model = "claude-sonnet-4",
+      },
+      vendors = {
+        copilot_gpt4_1 = {
+          __inherited_from = "copilot",
+          model = "gpt-4.1",
+        },
+        copilot_gemini = {
+          __inherited_from = "copilot",
+          model = "gemini-2.5-pro",
+        },
+      },
+    },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "echasnovski/mini.icons",
+      "zbirenbaum/copilot.lua",
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+  -- This is a compatibility layer for Blink sources
+  {
+    "saghen/blink.cmp",
+    lazy = true,
+    dependencies = { "saghen/blink.compat" },
+    opts = {
+      sources = {
+        default = { "avante_commands", "avante_mentions", "avante_files" },
+        compat = {
+          "avante_commands",
+          "avante_mentions",
+          "avante_files",
+        },
+        providers = {
+          avante_commands = {
+            name = "avante_commands",
+            module = "blink.compat.source",
+            score_offset = 90,
+            opts = {},
+          },
+          avante_files = {
+            name = "avante_files",
+            module = "blink.compat.source",
+            score_offset = 100,
+            opts = {},
+          },
+          avante_mentions = {
+            name = "avante_mentions",
+            module = "blink.compat.source",
+            score_offset = 1000,
+            opts = {},
+          },
+        },
+      },
+    },
+  },
+}
